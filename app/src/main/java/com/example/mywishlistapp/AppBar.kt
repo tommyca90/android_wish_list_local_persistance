24610e0dfc71b6c7af650b4ed043a492cf4db3a9
thomas.calabrese <thomascalabrese1@freenet.de>
2025-02-22 11:50:27 +0100

    added simple check to en/disable the back button in app bar

--- app/src/main/java/com/example/mywishlistapp/AppBar.kt
+++ app/src/main/java/com/example/mywishlistapp/AppBar.kt
@@ -2,10 +2,15 @@ package com.example.mywishlistapp
 
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -14,6 +19,23 @@ fun AppBarView(
     title: String,
     onBackNavClicked: () -> Unit = {}
 ) {
+    val navigationIcon: (@Composable () -> Unit)? =
+        if (title.contains("Wish List")) {
+            null
+        } else {
+            {
+                IconButton(
+                    onClick = { onBackNavClicked() }
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        tint = Color.White,
+                        contentDescription = "Back Arrow"
+                    )
+                }
+            }
+        }
+
     TopAppBar(
         title = {
             Text(
@@ -25,6 +47,7 @@ fun AppBarView(
             )
         },
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color)
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 }
