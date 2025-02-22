fc44c823eaf80ea95a27b4ba3346da07d180f867
thomas.calabrese <thomascalabrese1@freenet.de>
2025-02-22 12:14:12 +0100

    added a floating action button to HomeView

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -3,14 +3,36 @@ package com.example.mywishlistapp
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
-    Scaffold(topBar = { AppBarView(title = "Wish List") }) {
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)){
+    Scaffold(
+        topBar = { AppBarView(title = "Wish List") },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {}
+            ) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "Add")
+            }
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
 
         }
     }
