82740e110695132715113a69401f83925f8d8715
thomas.calabrese <thomascalabrese1@freenet.de>
2025-02-22 11:22:34 +0100

    added home view and display it in app

---
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -0,0 +1,17 @@
+package com.example.mywishlistapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeView() {
+    Scaffold(topBar = { AppBarView(title = "Wish List") }) {
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)){
+
+        }
+    }
+}
--- app/src/main/java/com/example/mywishlistapp/MainActivity.kt
+++ app/src/main/java/com/example/mywishlistapp/MainActivity.kt
@@ -3,45 +3,24 @@ package com.example.mywishlistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mywishlistapp.ui.theme.MyWishlistAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyWishlistAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeView()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyWishlistAppTheme {
-        Greeting("Android")
     }
 }
