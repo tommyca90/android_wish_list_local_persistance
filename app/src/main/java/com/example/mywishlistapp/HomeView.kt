1d29d32a56a20aa0ca46d2c81cef2f44c8bef6be
thomas.calabrese <thomascalabrese1@freenet.de>
2025-02-23 19:12:46 +0100

    enabled deleting a wish by swiping to left

--- app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,6 +1,9 @@
 package com.example.mywishlistapp
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,14 +11,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FractionalThreshold
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -23,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.mywishlistapp.data.Wish
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeView(navController: NavController, viewModel: WishViewModel) {
     Scaffold(
@@ -46,12 +59,40 @@ fun HomeView(navController: NavController, viewModel: WishViewModel) {
                 .fillMaxSize()
                 .padding(it)
         ) {
-            items(wishList.value){
-                wish -> WishItem(
-                    wish = wish,
-                    {
-                        val id = wish.id
-                        navController.navigate(Screen.AddScreen.route + "/$id")
+            items(wishList.value, key = { wish -> wish.id }) { wish ->
+                val dismissState = rememberDismissState(
+                    confirmStateChange = {
+                        if (it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart) {
+                            viewModel.deleteWish(wish)
+                        }
+                        true
+                    }
+                )
+                SwipeToDismiss(
+                    state = dismissState,
+                    background = {
+                        val color by animateColorAsState(
+                            if(dismissState.dismissDirection == DismissDirection.EndToStart) Color.Red else Color.Transparent,
+                            label = ""
+                        )
+                        val alignment = Alignment.CenterEnd
+                        Box(
+                            Modifier.fillMaxSize().background(color).padding(horizontal = 20.dp),
+                            contentAlignment = alignment
+                        ){
+                            Icon(Icons.Default.Delete, contentDescription = "delete wish", tint = Color.White)
+                        }
+                    },
+                    directions = setOf(DismissDirection.EndToStart),
+                    dismissThresholds = { FractionalThreshold(0.25f) },
+                    dismissContent = {
+                        WishItem(
+                            wish = wish,
+                            {
+                                val id = wish.id
+                                navController.navigate(Screen.AddScreen.route + "/$id")
+                            }
+                        )
                     }
                 )
             }
